5a27235e313a069eeac5cace28d9553b7bcf0aeb
Rodolfo Neto <rodolfoneto>
2022-04-22 12:13:24 -0300

    ---

--- polen/plugins/polen_2/api/v2/Api_Polen_Home.php
+++ polen/plugins/polen_2/api/v2/Api_Polen_Home.php
@@ -2,9 +2,39 @@
 
 namespace Polen\Api\v2;
 
+use WP_REST_Request;
+use WP_REST_Server;
+
 defined('ABSPATH') || die;
 
 class Api_Polen_Home
 {
-    
+    public function __construct()
+    {
+        $this->namespace = 'polen/v2';
+        $this->rest_base = 'home';
+
+        add_action('rest_api_init', [$this, 'register_routes']);
+    }
+
+    public function register_routes()
+    {
+        register_rest_route($this->namespace, $this->rest_base . '/uses_case', [
+            [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'get_uses_cases'],
+                'permission_callback' => "__return_true",
+                'args' => []
+            ],
+        ] );
+    }
+
+
+    /**
+     * 
+     */
+    public function get_uses_cases(WP_REST_Request $request)
+    {
+        
+    }
 }
