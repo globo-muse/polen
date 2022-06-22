3dbe5797c7005e9149c4f519802265bbfab213f3
Glaydson Rodrigues <glaydson012@gmail.com>
2022-06-22 17:48:30 -0300

    Adicionar possiblidade de filtrar por multiplas faixas de idade

--- polen/plugins/polen_2/api/b2b/checkout/Api_Checkout.php
+++ polen/plugins/polen_2/api/b2b/checkout/Api_Checkout.php
@@ -221,6 +221,12 @@ class Api_Checkout extends WP_REST_Controller
     {
         try {
             $b2b_order = new Polen_B2B_Orders($request['order_id'], $request['key_order']);
+            $order = wc_get_order($request['order_id']);
+
+            $status_payment = ['payment-approved', 'video-sended', 'completed'];
+            if (in_array($order->get_status(), $status_payment)) {
+                throw new Exception('Esse pedido já foi pago', 406);
+            }
 
             return api_response($b2b_order->get_order_info_step_one());
         } catch(Exception $e) {
